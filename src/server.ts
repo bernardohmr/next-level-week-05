2897a667a28f80e9d9641113aa3d657a3315c1f7
Bernardo Rodrigues <bernardohmr@gmail.com>
2021-04-25 19:54:20 -0300

    initial

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { http } from "./http";
+
+import "./websocket/client";
+
+const PORT = 3333;
+
+http.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ src/services/SettingsService.ts
@@ -0,0 +1,58 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
+import { SettingsRepository } from "../repositories/SettingsRepository";
+
+interface ISettingsCreate {
+    chat: boolean;
+    username: string;
+}
+
+class SettingsService {
+    private settingsRepository: Repository<Setting>;
+
+    constructor() {
+        this.settingsRepository = getCustomRepository(SettingsRepository);
+    }
+
+    async create({
+        chat,
+        username,
+    }: ISettingsCreate) {
+        const existingUser = await this.settingsRepository.findOne({ username });
+
+        if (!!existingUser) {
+            throw new Error("Usuário já existe");
+        }
+    
+        const settings = this.settingsRepository.create({
+            chat,
+            username,
+        });
+    
+        await this.settingsRepository.save(settings);
+
+        return settings;
+    }
+
+    async findByUsername(username: string) {
+        const settings = await this.settingsRepository.findOne({
+            username,
+        });
+
+        return settings;
+    }
+
+    async update(username: string, chat: boolean) {
+        const settings = await this.settingsRepository.createQueryBuilder()
+            .update(Setting)
+            .set({ chat })
+            .where("username = :username", {
+                username,
+            })
+            .execute();
+
+        return settings;
+    }
+}
+
+export { SettingsService }
